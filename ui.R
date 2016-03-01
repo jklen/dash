4d7330fb2196d3998d60aa9c4feb384cf4de4879
jklen <klen.jaro@gmail.com>
2016-03-01 15:44:29 +0100

    deleted default stuff

--- ui.R
+++ ui.R
@@ -3,33 +3,12 @@
 # You can find out more about building applications with Shiny here:
 #
 # http://shiny.rstudio.com
-#
 
-library(shiny)
-
-# shinyUI(fluidPage(
-# 
-#   # Application title
-#   titlePanel("Old Faithful Geyser Data"),
-# 
-#   # Sidebar with a slider input for number of bins
-#   sidebarLayout(
-#     sidebarPanel(
-#       sliderInput("bins",
-#                   "Number of bins:",
-#                   min = 1,
-#                   max = 50,
-#                   value = 30)
-#     ),
-# 
-#     # Show a plot of the generated distribution
-#     mainPanel(
-#       plotOutput("distPlot")
-#     )
-#   )
-# ))
 
 library(shiny) # load shiny at beginning at both scripts
+library(plotly)
+library(ggplot2)
+library(dplyr)
 
 shinyUI(pageWithSidebar( # standard shiny layout, controls on the
   # left, output on the right
